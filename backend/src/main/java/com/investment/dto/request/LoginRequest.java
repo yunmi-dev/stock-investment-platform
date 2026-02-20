3d5678a42941061e85faaf2d2400102b48252a17
Jeong-Yunmi <yunmiii0226@khu.ac.kr>
2026-02-21 01:08:16 +0900

    feat: DTO 클래스 추가

---
+++ backend/src/main/java/com/investment/dto/request/LoginRequest.java
@@ -0,0 +1,17 @@
+package com.investment.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+
+@Getter
+public class LoginRequest {
+
+    @NotBlank(message = "이메일을 입력해주세요")
+    @Email(message = "이메일 형식이 올바르지 않습니다")
+    private String email;
+
+    @NotBlank(message = "비밀번호를 입력해주세요")
+    private String password;
+}
---
+++ backend/src/main/java/com/investment/dto/response/AuthResponse.java
@@ -0,0 +1,12 @@
+package com.investment.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class AuthResponse {
+    private String token;
+    private String email;
+    private String name;
+}
